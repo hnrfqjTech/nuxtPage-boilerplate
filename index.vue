99b6bea5cc90f14b9c6c7b5fa99ef92ba89812dd
bigang.ybg <93745726@qq.com>
2018-12-31 23:06:04 +0800

    feat: init

---
+++ index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="demo-page">
+    示例
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  mixins: [],
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  destroyed() {},
+  methods: {
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.demo-page {
+
+}
+</style>
